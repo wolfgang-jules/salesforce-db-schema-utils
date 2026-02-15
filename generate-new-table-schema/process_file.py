8d6ca944783d57cff87a521b40a3e6a4fad367b6
wolfgang-jules <133794525+wolfgang-jules@users.noreply.github.com>
2026-02-15 15:06:05 -0600

    default params

--- generate-new-table-schema/process_file.py
+++ generate-new-table-schema/process_file.py
@@ -4,6 +4,6 @@ It reads the input CSV file, which contains the table schema, from the "input_fi
 folder and analyzes the data to generate new field names. The output is a new CSV file
 with a new column named "NewFieldName", and stored in the "output_files" folder.
 """
-from module import process_file
+from shared import process_file
 
-process_file('AHJRequirement__c')
+process_file('asset')
--- generate-new-table-schema/validate_file.py
+++ generate-new-table-schema/validate_file.py
@@ -4,6 +4,6 @@ It checks if the values of the "NewFieldName" column were generated correctly,
 comparing the old field name, label name, and new field name, and depending of the
 findings will print the messages with different colors for better understanding.
 """
-from module import validate_file
+from shared import validate_file
 
-validate_file('AHJRequirement__c_final')
+validate_file('asset_final_version')
